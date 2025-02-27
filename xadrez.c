775bddd09a7c4c81643af05a1bb4255150d2d56b
Gabriel B Lisboa <gabrielblisboa1800@gmail.com>
2025-02-27 23:35:54 +0000

    Segundo teste, movimento

--- xadrez.c
+++ xadrez.c
@@ -3,17 +3,26 @@
 int main(){
 
    int numero_peca;
+   int direcao;
 
-   printf("Escolha uma Peça para mover: ");
-   printf("Bispo(1)\n");
-   printf("Rainha(2)\n");
-   printf("Torre(3)\n");
+   printf("Escolha uma Peça para mover:\n");
+   printf("Bispo (1)\n");
+   printf("Rainha (2)\n");
+   printf("Torre (3)\n");
 
    scanf("%d", &numero_peca);
 
+   if(numero_peca == 1){
+      printf("Escolha a direçao que o Bispo se move-ra: ");
+      printf("Diagonal Superior Esquerda (1)\n");
+      printf("Diagonal Superior Direita (2)\n");
+      printf("Diagonal Inferior Esquerda (3)\n");
+      printf("Diagonal inferior Direita (4)\n");
 
-   for(int i = 0; i < 5; i++){
-      printf("Direita\n");
+      scanf("%d", &direcao);
+   }else{
+      printf("Direçao nao encontrada! ");
    }
+
    return 0;
 }
