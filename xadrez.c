352e6ce6b5ad8d80b6adce18970e3e25eeecd4a2
Gabriel B Lisboa <gabrielblisboa1800@gmail.com>
2025-03-09 12:57:51 +0000

    Movimento do bispo criado, usando do while

--- xadrez.c
+++ xadrez.c
@@ -2,27 +2,14 @@
 
 int main(){
 
-   int numero_peca;
-   int direcao;
+   int i;
 
-   printf("Escolha uma Peça para mover:\n");
-   printf("Bispo (1)\n");
-   printf("Rainha (2)\n");
-   printf("Torre (3)\n");
-
-   scanf("%d", &numero_peca);
-
-   if(numero_peca == 1){
-      printf("Escolha a direçao que o Bispo se move-ra: ");
-      printf("Diagonal Superior Esquerda (1)\n");
-      printf("Diagonal Superior Direita (2)\n");
-      printf("Diagonal Inferior Esquerda (3)\n");
-      printf("Diagonal inferior Direita (4)\n");
-
-      scanf("%d", &direcao);
-   }else{
-      printf("Direçao nao encontrada! ");
-   }
+   printf("Movimento do Bispo:\n");
+   i = 0;
+   do{
+      printf("Cima-Direita\n");
+      i++;
+   }while (i < 5);
 
    return 0;
 }
